fe426d2b1f363efd547eaa83acba25ac59a76ec9
Robin Stuart <robin@zint.org.uk>
2009-08-15 17:22:53 +0000

    Update license info

--- backend/common.h
+++ backend/common.h
@@ -2,11 +2,11 @@
 
 /*
     libzint - the open source barcode library
-    Copyright (C) 2008 Robin Stuart <robin@zint.org.uk>
+    Copyright (C) 2009 Robin Stuart <robin@zint.org.uk>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
